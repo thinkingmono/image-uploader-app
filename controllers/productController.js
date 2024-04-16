83d5be898f2fe65721d309127f314b2088f052b4
Alejandro Garcia <thinkingmono92@gmail.com>
2024-04-15 22:05:51 -0500

    Image Upload | Project files upload

---
+++ controllers/productController.js
@@ -0,0 +1,16 @@
+const { StatusCodes } = require('http-status-codes')
+const Product = require('../models/Product')
+
+
+const createProduct = async (req, res) => {
+    // console.log(req.body);
+    const product = await Product.create(req.body)
+    res.status(StatusCodes.CREATED).json(product)
+}
+
+const getAllProducts = async (req, res) => {
+    const products = await Product.find({})
+    res.status(StatusCodes.OK).json({ products })
+}
+
+module.exports = { createProduct, getAllProducts }
